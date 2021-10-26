11d2e740174d63972013d7ebc47f20d1f3d1c17a
dsleylian <daryaleylian@gmail.com>
2021-10-26 15:34:56 -0700

    file path correction again with /

--- src/Data/aboutUsSettingsData.js
+++ src/Data/aboutUsSettingsData.js
@@ -1,25 +1,25 @@
 export const aboutUsSettingData = [
     { 
         id: 0,
-        image: "images/inhomesetting.svg",
+        image: "/images/inhomesetting.svg",
         alt: "in home therapy",
         title: "In Home",
     },
     { 
         id: 1,
-        image: "images/building.svg",
+        image: "/images/building.svg",
         alt: "in office therapy",
         title: "In Our Office",
     },
     { 
         id: 2,
-        image: "images/schoollocation.svg",
+        image: "/images/schoollocation.svg",
         alt: "on site therapy",
         title: "On Site",
     },
     { 
         id: 4,
-        image: "images/telehealth.svg",
+        image: "/images/telehealth.svg",
         alt: "tele-therapy",
         title: "Tele-Therapy",
     },
