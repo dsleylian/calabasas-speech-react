7dab6cc3eb2eaaf72c70ccd8a3394e082c789072
dsleylian <daryaleylian@gmail.com>
2021-10-27 23:33:29 -0700

    trying different paths

--- src/Data/homePageData.js
+++ src/Data/homePageData.js
@@ -3,7 +3,7 @@ export const homePageData = [
         id: 0,
         title: "Pediatric and Adult",
         content: "We work with all ages, from early intervention to adults. Our clinicians have vast experience with different age groups, as well as with varied areas of need including fluency, speech sound disorders, and language delays.",
-        image: "../../images/readbook.jpg",
+        image: "images/readbook.jpg",
         alt: "Clinician reading a book with a client"
 
     },
@@ -11,7 +11,7 @@ export const homePageData = [
         id: 1,
         title: "Family Oriented Approach",
         content: `"It takes a village to raise a child." We believe that our clients are part of a community, a family, and in order to see changes in your or your child's speech and language skills, an approach that targets and involves the entire village is required.`,
-        image: "../../images/holdgirl.jpg",
+        image: "./images/holdgirl.jpg",
         alt: "Clincian holding child"
 
     },
@@ -19,7 +19,7 @@ export const homePageData = [
         id: 2,
         title: "In Home Therapy",
         content: "Research has shown that therapy done in the child's most natural environemnt produces the best results. For most children, this environment is the home. We offer in-home therapy for those interested.",
-        image: "../../images/levelonfloor.jpg",
+        image: "/images/levelonfloor.jpg",
         alt: "Clinician sitting on the floor with client"
 
     },
